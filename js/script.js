b6440c88b5542181eb87807551f20a07e79e4cb8
asanheyaa <122150617+asanheyaa@users.noreply.github.com>
2024-11-15 17:27:12 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,74 +1,71 @@
 let amount = 1
 
-const tl = gsap.timeline({});
-tl.to(".pae__pixel_item", {
-	background: "#0ce402",
-	duration: .1,
-	delay: 3,
-	stagger: {
-		amount: amount,
-		grid: "auto",
-		from: "center",
-		// onComplete: function () {
-		// 	let df = this.targets()[0]
-		// 	gsap.to(df, {
-		// 		opacity: 0,
-		// 		duration: 1
-		// 	})
-		// }
+
+const showButtton = document.querySelector('.pae__button_show'),
+	hideButton = document.querySelector('.pae__button_hide')
+showButtton.addEventListener('click', (e)=>{
+	const tl = gsap.timeline({});
+	tl.to(".pae__pixel_item", {
+		background: "#0ce402",
+		duration: .1,
+		stagger: {
+			amount: amount,
+			grid: "auto",
+			from: "center",
+		},
+
+	}).to(".pae__pixel_item", {
+		background: "#e63037",
+		duration: .1,
+		stagger: {
+			amount: amount,
+			grid: "auto",
+			from: "random",
+		},
+
+	}, "<").to(".pae__pixel_item", {
+		background: "#f3fd7a",
+		duration: .1,
+		stagger: {
+			amount: amount,
+			grid: "auto",
+			from: "random",
+		},
+
+	}, "<").to(".pae__pixel_item", {
+		opacity: 0,
+		duration: .1,
+		stagger: {
+			amount: amount,
+			grid: "auto",
+			from: "random",
+		},
+
+	}, "<") 
+
+})
+
+hideButton.addEventListener('click', (e) => {
+	const tl = gsap.timeline({});
+	tl.to(".pae__pixel_item", {
+		opacity: 1,
+		duration: .1,
+		stagger: {
+			amount: amount,
+			grid: "auto",
+			from: "random",
+		},
+
+	}).to(".pae__pixel_item", {
+		background: "#FFF",
+		duration: .1,
+		stagger: {
+			amount: amount,
+			grid: "auto",
+			from: "random",
+		},
+
+	}, "<")
 		
-	},
-	
-}).to(".pae__pixel_item", {
-	background: "#e63037",
-	duration: .1,
-	stagger: {
-		amount: amount,
-		grid: "auto",
-		from: "random",
-		// onComplete: function () {
-		// 	let df = this.targets()[0]
-		// 	gsap.to(df, {
-		// 		opacity: 0,
-		// 		duration: 1
-		// 	})
-		// }
 
-	},
-
-}, "<").to(".pae__pixel_item", {
-	background: "#f3fd7a",
-	duration: .1,
-	stagger: {
-		amount: amount,
-		grid: "auto",
-		from: "random",
-		// onComplete: function () {
-		// 	let df = this.targets()[0]
-		// 	gsap.to(df, {
-		// 		opacity: 0,
-		// 		duration: 1
-		// 	})
-		// }
-
-	},
-
-}, "<").to(".pae__pixel_item", {
-	opacity:0,
-	duration: .1,
-	stagger: {
-		amount: amount,
-		grid: "auto",
-		from: "random",
-		// onComplete: function () {
-		// 	let df = this.targets()[0]
-		// 	gsap.to(df, {
-		// 		opacity: 0,
-		// 		duration: 1
-		// 	})
-		// }
-
-	},
-
-}, "<") 
-
+})
